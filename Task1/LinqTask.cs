530584acc8b06ad5bf328462d336ebdef4f731a3
Aliaksei Mukhamiadziyeu <Aliaksei_Mukhamiadziyeu@epam.com>
2022-07-25 22:27:19 +0400

    Tasks 1-6 done

--- Task1/LinqTask.cs
+++ Task1/LinqTask.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Task1.DoNotChange;
 
 namespace Task1
@@ -8,7 +10,7 @@ namespace Task1
     {
         public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
         {
-            throw new NotImplementedException();
+            return customers.Where(c => c.Orders.Sum(o => o.Total) > limit);
         }
 
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2(
@@ -16,7 +18,7 @@ namespace Task1
             IEnumerable<Supplier> suppliers
         )
         {
-            throw new NotImplementedException();
+            return customers.Select(c => (c, suppliers.Where(s => s.Country == c.Country && s.City == c.City)));
         }
 
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
@@ -24,31 +26,42 @@ namespace Task1
             IEnumerable<Supplier> suppliers
         )
         {
-            throw new NotImplementedException();
+            return customers.Select(c => (c, suppliers.Where(s => s.Country == c.Country && s.City == c.City)));
         }
 
         public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
         {
-            throw new NotImplementedException();
+            return customers.Where(c => c.Orders.Sum(o => o.Total) > limit && c.Orders.Length > 0);
         }
 
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq4(
             IEnumerable<Customer> customers
         )
         {
-            throw new NotImplementedException();
+            return customers.Select(
+                c => (c, c.Orders.Select(o => o.OrderDate).FirstOrDefault(d => d == c.Orders.Min(o => o.OrderDate))))
+                .Where(t => t.c.Orders.Length != 0);
         }
 
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
             IEnumerable<Customer> customers
         )
         {
-            throw new NotImplementedException();
+            return customers.Select(
+                    c => (c, c.Orders.Select(o => o.OrderDate).FirstOrDefault(d => d == c.Orders.Min(o => o.OrderDate))))
+                .Where(t => t.c.Orders.Length != 0)
+                .OrderBy(t => t.c.Orders.FirstOrDefault().OrderDate.Year)
+                .ThenBy(t => t.c.Orders.FirstOrDefault().OrderDate.Month)
+                .ThenByDescending(t => t.c.Orders.FirstOrDefault().Total)
+                .ThenBy(t => t.c.CompanyName);
         }
 
         public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
         {
-            throw new NotImplementedException();
+            Regex postalRegex = new Regex("^\\d+$");
+            Regex phoneRegex = new Regex("\\(([^\\)]+)\\)");
+            return customers.Select(c => c).Where(c =>
+                !postalRegex.IsMatch(c.PostalCode) || string.IsNullOrEmpty(c.Region) || !phoneRegex.IsMatch(c.Phone));
         }
 
         public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
@@ -63,7 +76,11 @@ namespace Task1
 		            price - 18.0000
 		            price - 19.0000
              */
+            //var result = products
+            //    .GroupBy(p => new { p.Category, p.UnitsInStock, price = p.UnitPrice })
+            //    .OrderBy(p => p.Key.price);
 
+            //return result;
             throw new NotImplementedException();
         }
 
